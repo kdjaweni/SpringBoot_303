912f8d3c61b3d907f83a27a9333337fd5422380f
kdjaweni <kdjaweni@gmail.com>
2021-08-13 10:18:32 -0400

    first commit

---
+++ src/test/java/com/example/springboot_303/SpringBoot303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot303ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
